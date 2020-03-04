81eb3e3fb87e1095f2409b42ff3ba45885745316
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-03 17:06:51 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      {/* content here */}
+    </main>
+  );
+}
+
+export default App;
